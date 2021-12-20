04d0babea76985702207b1c285502824896f7656
Edgardo Infante <63029643+edgardo29@users.noreply.github.com>
2021-12-19 20:24:47 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -10,7 +10,7 @@
      $txt = "You have received an e-mail from ".$name.".\n\n".$message;
      
      mail($mailTo, $subject, $txt, $headers);
-     header("Location: index.php?mailsend");
+     header("Location: index.html");
  }
 
 
